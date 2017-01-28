078eca4bc3cb6557ac2b3da833315ca10151f13a
lucero-develops <mike.j.lucero@gmail.com>
2017-01-28 17:50:55 -0500

    Starter code

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: 99 Bottles of Juice (4-2)
+ */
+
+var num = 99;
+
+while (/* your stop condition goes here */) {
+    // check value of num
+    // print lyrics using num
+    // don't forget to check pluralization on the last line!
+    // decrement num
+}
